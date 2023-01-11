82cb1007f735eb90418fe7e8f0f96be507e2a46f
Ruben Giosa <rubenalexgiosa@gmail.com>
2023-01-11 15:31:50 -0800

    add to schema yml file for fct_tickets

--- demo_dbt/models/staging/stg_tickets.sql
+++ demo_dbt/models/staging/stg_tickets.sql
@@ -13,7 +13,7 @@ with tickets as (
         passenger.email as email_id,
     FROM {{ source('air_travel', 'air_travel') }}
     GROUP BY 1,2,3,4,5,6,7,8,9,10
-)
+),
 
 consolidated_tickets as (
     SELECT *
@@ -23,8 +23,7 @@ consolidated_tickets as (
         FROM {{ ref('stg_passengers') }} 
         ) as p
     ON tickets.email_id = p.email_id
-
-)
+),
 
 final as (
     SELECT
