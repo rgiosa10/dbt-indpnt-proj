0305db5ff950cbd41fa63b517b14fb5013d2fd63
Ruben Giosa <rubenalexgiosa@gmail.com>
2023-01-13 12:38:04 -0800

    WIP: continuing to work on merge of ticket updates

--- demo_dbt/models/staging/stg_ticket_updates.sql
+++ demo_dbt/models/staging/stg_ticket_updates.sql
@@ -1,11 +1,3 @@
-{{
-    config(
-        materialized='incremental',
-        unique_key='eticket_num',
-        on_schema_change='fail'
-    )
-}}
-
 with ticket_updates as (
     SELECT *
     FROM {{ source('air_travel', 'ticket_updates') }}
@@ -26,58 +18,8 @@ consolidated as (
         FROM {{ ref('stg_tickets') }} 
         ) as stgt
     ON ticket_updates.eticket_num = stgt.eticket_num
-),
-
-using_clause as (
-
-    SELECT
-        price,
-        seat,
-        status
-    FROM consolidated
-
-    {% if is_incremental() %}
-
-        WHERE price != (SELECT price FROM {{ this }}) or seat != (SELECT seat FROM {{ this }}) or status != (SELECT status FROM {{ this }})
-
-    {% endif %}
-),
-
-updates as (
-    SELECT
-        price,
-        seat,
-        status,
-        CURRENT_TIMESTAMP as modified_at
-    FROM using_clause
-
-    {% if is_incremental() %}
-
-        WHERE eticket_num IN (SELECT eticket_num FROM {{ this }})
-
-    {% endif %}
-
-),
-
-inserts AS (
-
-    SELECT
-        eticket_num,
-        confirmation,
-        ticket_date,
-        price,
-        seat,
-        status,
-        origin_iata, 
-        dest_iata, 
-        airline_iata,
-        passenger_sk,
-        created_at,
-        modified_at
-    FROM using_clause
-    WHERE eticket_num NOT IN (SELECT eticket_num FROM updates)
-
 )
 
-SELECT * FROM updates UNION ALL SELECT * FROM inserts
+SELECT *
+FROM consolidated
     
